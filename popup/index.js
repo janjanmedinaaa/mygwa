a159ab52063d77821a51f19f6a4bae2e31180211
Janjan Medina <medinajuanantonio95@gmail.com>
2018-07-15 09:01:19 +0800

    Add toggle switch onClick Listener

--- popup/index.js
+++ popup/index.js
@@ -1,9 +1,9 @@
-window.onload = function() {
+window.onload = () => {
 
     chrome.storage.sync.get(['data'], (response) => { 
         var name = document.getElementsByClassName('user-name');
         var degree = document.getElementsByClassName('user-degree');
-        var image = document.getElementsByClassName('profile-picture');
+        var image = document.getElementById('profile-picture');
 
         console.log(name, degree, image);
 
@@ -14,7 +14,48 @@ window.onload = function() {
         else{
             name[0].innerHTML = response.data.name;
             degree[0].innerHTML = response.data.degree;
-            image[0].src = response.data.image;
+            image.src = response.data.image;
         }
     });
 }
+
+window.addEventListener('load', () => {
+    var switchToggle = document.getElementById('switch');
+
+    var params = {
+        active: true,
+        currentWindow: true,
+    }
+
+    switchToggle.onclick = function() {
+        if (switchToggle.checked == true){
+            var msg = {
+                type: "pressure-filter-on",
+                title: "Turn on Pressure Filter",
+                message: "Please turn on Pressure Filter"
+            }
+
+            chrome.tabs.query(params, (tabs) => {
+                chrome.tabs.sendMessage(tabs[0].id, msg, (response) => {
+                    console.log(response);
+                });
+            });
+
+        } 
+        else {
+            var msg = {
+                type: "pressure-filter-off",
+                title: "Turn off Pressure Filter",
+                message: "Please turn off Pressure Filter"
+            }
+
+            chrome.tabs.query(params, (tabs) => {
+                chrome.tabs.sendMessage(tabs[0].id, msg, (response) => {
+                    console.log(response);
+                });
+            });
+        }
+
+        console.log(switchToggle.checked);
+    }
+})
