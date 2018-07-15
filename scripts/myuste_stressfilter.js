d48e8b32f58f627ca17c022936b4d43492f4df5f
Janjan Medina <medinajuanantonio95@gmail.com>
2018-07-16 02:20:38 +0800

    Clean Code. Copy currentSemesterView and fixed bug

--- scripts/myuste_stressfilter.js
+++ scripts/myuste_stressfilter.js
@@ -1,73 +1,102 @@
-function stressFilter(status){
-    var tables = document.getElementsByTagName('table');
-    var gradesTableId = null;
+function dataScrape(){
+    let tables = document.getElementsByTagName('table');
+    let gradesTableId = null;
+    let tableTr = [];
 
-    var tableFinalData = [],
-        tableTr = [];
-
-    for(var a = 0; a < tables.length; a++){
+    for(let a = 0; a < tables.length; a++){
         if(tables[a].id != "sysem_list" && tables[a].id != "grades_table" && tables[a].id != ""){
             gradesTableId = a;
         }
     }
 
-    var tableInitialData = tables[gradesTableId].rows;
-    var tableDataLength = tableInitialData.length;
+    let tableInitialData = tables[gradesTableId].rows;
+    let tableDataLength = tableInitialData.length;
 
-    for(var b = 0; b < tableDataLength; b++){
+    for(let b = 0; b < tableDataLength; b++){
         tableTr.push(tableInitialData[b]);
     }
 
+    return tableTr;
+}
+
+function filterData(tableTr, status){
     for(var c = 1; c < tableTr.length; c++){
-        var tableTd = [];
         var d = 4;
         
         while(d < tableTr[c]['children'].length){
-            var prelim = tableTr[c]['children'][d].innerHTML;
-            var final = tableTr[c]['children'][d+1].innerHTML;
+            var prelim = tableTr[c]['children'][d];
+            var final = tableTr[c]['children'][d+1];
 
-            tableTd.push(prelim, final);
-            if(status == "on"){
-                if(parseInt(prelim) >= 75){
-                    tableTr[c]['children'][d].innerHTML = "PASSED";
-                    tableTr[c]['children'][d].style.color = "green";
-                }
-                else if(prelim === "&nbsp;"){
-                    tableTr[c]['children'][d].innerHTML = "NO GRADE";
-                }
-                else{
-                    tableTr[c]['children'][d].innerHTML = "FAILED";
-                    tableTr[c]['children'][d].style.color = "red";
-                }
-    
-                if(parseFloat(final) <= 3.0){
-                    tableTr[c]['children'][d+1].innerHTML = "PASSED";
-                    tableTr[c]['children'][d+1].style.color = "green";
-                }
-                else if(final === "&nbsp;"){
-                    tableTr[c]['children'][d+1].innerHTML = "NO GRADE";
-                }
-                else{
-                    tableTr[c]['children'][d+1].innerHTML = "FAILED";
-                    tableTr[c]['children'][d+1].style.color = "red";
-                }
-            }
+            (status == "on") ? filter(prelim, final) : unfilter(prelim, final, c);
+
             d += 6;
         }
-
-        tableFinalData.push(tableTd);
     }
-
-    return tableFinalData;
 }
 
-// console.log(stressFilter());
-
-chrome.runtime.onMessage.addListener((message, sender, response) => {
-    if(message.type == "pressure-filter-on"){
-        stressFilter("on");
+function filter(prelim, final){
+    if(parseInt(prelim.innerHTML) >= 75){
+        prelim.innerHTML = "PASSED";
+        prelim.style.color = "green";
+    }
+    else if(prelim.innerHTML === "&nbsp;"){
+        prelim.innerHTML = "NO GRADE";
+    }
+    else{
+        prelim.innerHTML = "FAILED";
+        prelim.style.color = "red";
     }
 
-    console.log(message);
-    response("Received Message");
-})
+    if(parseFloat(final.innerHTML) <= 3.0){
+        final.innerHTML = "PASSED";
+        final.style.color = "green";
+    }
+    else if(final.innerHTML === "&nbsp;"){
+        final.innerHTML = "NO GRADE";
+    }
+    else{
+        final.innerHTML = "FAILED";
+        final.style.color = "red";
+    }
+}
+
+function unfilter(prelim, final, c){
+    let currentsem = currentSemesterView();
+
+    chrome.storage.sync.get([currentsem], (response) => {
+        let userData = response[currentsem]['data']['details']['userData'];
+
+        prelim.innerHTML = userData[c][4];
+        final.innerHTML = userData[c][5];
+        prelim.style.color = "black";
+        final.style.color = "black";
+    })
+}
+
+function currentSemesterView(){
+    let container = document.getElementById('container_body_right');
+    let semText = container.childNodes[7].innerHTML;
+    let semId = semText.replace("&nbsp;&nbsp;", "").trim();
+
+    return semId;
+}
+
+chrome.runtime.onMessage.addListener((message, sender, response) => {
+    let messageReturn = null;
+
+    if(message.type == "pressure-filter-on"){
+        filterData(dataScrape(), "on");
+        messageReturn = true;
+    }
+    else if(message.type == "pressure-filter-off"){
+        filterData(dataScrape(), "off");
+        messageReturn = false;
+    }
+
+    response(messageReturn);
+});
+
+chrome.storage.sync.get(['toggle'], (response) => {
+    console.log(response.toggle);
+    filterData(dataScrape(), response.toggle);
+});
